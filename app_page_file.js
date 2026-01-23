875420a68d33dd96de892f57449ba763cfb912cc
LenLi77 <helenlaasik@gmail.com>
2026-01-23 21:51:06 +0200

    Delete app_page_file.js

--- app_page_file.js
+++
@@ -1,9 +0,0 @@
-import TotalCompensationCalculator from '@/components/TotalCompensationCalculator';
-
-export default function Home() {
-  return (
-    <main>
-      <TotalCompensationCalculator />
-    </main>
-  );
-}
